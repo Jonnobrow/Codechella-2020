edc729b862a67bb8f8e82abc8c308966fe59f0fe
ShreehariVaasishta <shreehari9481@gmail.com>
2020-11-22 11:46:36 +0530

    Add follow user func
    also remove some whitespaces

--- lambda/twitter_functions.py
+++ lambda/twitter_functions.py
@@ -14,7 +14,7 @@ class TwitterFunctions():
         user = self.api.get_user(username)
         latest_tweets = [status.text for status in self.api.user_timeline(screen_name=user.screen_name, count=5)]
         return latest_tweets
-        
+
     def getTrendingTopics(self):
         # WID 1 for global trends
         trends = self.api.trends_place(id=1)
@@ -24,10 +24,18 @@ class TwitterFunctions():
                 result.append(trend['name'])
                 
         return result
-                
+
     def getTopicTweets(self, topic):
         topic.replace('hashtag', '#')
         if topic[0] == '#':
             return [status.text for status in self.api.search(topic)]
         else:
             return [status.text for status in self.api.search('#' + topic)]
+
+    def followUser(self, username):
+        try:
+            user = self.api.get_user(username)
+            user.create_friendship(screen_name=user.screen_name)
+            return True
+        except Exception as e:
+            return False
